b4ce185ee1637448f305f4c619fd099474b908cb
Zaira Bibi <zaira.bibi@A006-01279.local>
2025-06-23 14:17:05 +0500

    feat: Add plugins/extension for prompt-generation
    The functionality for prompt generation was not customizable. The developers were not able to modify the arguments without having access to the
    core code. This limited the flexibility of the application and made it difficult to adapt to different use cases.
    
    Fixed that by implementing a plugin architecture that allows users to extend the functionality of the application by creating plugins.
    This allows users to modify the arguments passed to the prompt generation function without having to modify the core code.

---
+++ src/course_args.rs
@@ -0,0 +1,85 @@
+use std::{collections::HashMap};
+
+pub trait Plugin<T> {
+    fn name(&self) -> &str;
+    fn transform(&self, data: &mut T) -> Result<(), String>;
+}
+
+pub struct PluginManager<T> {
+    plugins: HashMap<String, Box<dyn Plugin<T>>>,
+    execution_order: Vec<String>,
+}
+
+impl<T> Default for PluginManager<T> {
+    fn default() -> Self {
+        Self {
+            plugins: HashMap::new(),
+            execution_order: Vec::new(),
+        }
+    }
+}
+
+impl<T> PluginManager<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    
+    pub fn register<P: Plugin<T> + 'static>(&mut self, plugin: P) -> Result<(), String> {
+        let name = plugin.name().to_string();
+        self.plugins.insert(name.clone(), Box::new(plugin));
+
+        if !self.execution_order.contains(&name) {
+            self.execution_order.push(name);
+        }
+        
+        Ok(())
+    }
+    
+    pub fn process(&self, mut data: T) -> Result<T, String> {
+        // TODO: Use indexing instead of vector for execution order
+        for plugin_name in self.execution_order.iter() {
+            if let Some(plugin) = self.plugins.get(plugin_name) {
+                plugin.transform(&mut data)?;
+            }
+        }
+        
+        Ok(data)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct CourseArgs {
+    pub course_duration: String,
+    pub course_name: String,
+    pub course_description: String,
+    pub others: HashMap<String, String>,
+}
+
+pub struct AssessmentPlugin;
+
+impl Plugin<CourseArgs> for AssessmentPlugin {
+    // 
+    fn name(&self) -> &str {
+        "assessment_plugin"
+    }
+    
+    fn transform(&self, data: &mut CourseArgs) -> Result<(), String> {
+        data.others.insert("include_assessments".to_string(), "true".to_string());
+        Ok(())
+    }
+}
+
+pub struct ModifyCourseNamePlugin;
+
+impl Plugin<CourseArgs> for ModifyCourseNamePlugin {
+    fn name(&self) -> &str {
+        "modify_course_name_plugin"
+    }
+    
+    fn transform(&self, data: &mut CourseArgs) -> Result<(), String> {
+        data.course_name = String::from("new course name");
+        Ok(())
+    }
+}
+
+
