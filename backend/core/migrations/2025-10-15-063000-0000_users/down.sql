1570b85e574e1bc66ae32490d300e5fa9ad61ab7
Zaira Bibi <zaira.bibi@A006-01773.local>
2025-10-17 12:19:51 +0500

    refactor: rename `core` lib
    Naming the crate `core` conflicts with the standard library's own core crate,
    which is always in scope.
    
    Renamed it to `app_core` so that it doesn't conflict with Rust's core libraries

--- backend/core/migrations/2025-10-15-063000-0000_users/down.sql
+++
@@ -1,6 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP INDEX IF EXISTS idx_users_email;
-DROP INDEX IF EXISTS idx_users_username;
-DROP INDEX IF EXISTS idx_users_id;
-
-DROP TABLE IF EXISTS users;
