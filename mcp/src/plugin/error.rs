621a4c76e7609d60ee9642dd9ae4fca338532de2
Zaira Bibi <zaira.bibi@A006-01773.local>
2025-10-22 14:25:23 +0500

    fix: removed `mcp/src/error.rs` and commented code

--- mcp/src/plugin/error.rs
+++ mcp/src/plugin/error.rs
@@ -4,20 +4,6 @@ pub type Result<T> = std::result::Result<T, PluginError>;
 
 #[derive(Debug, Error)]
 pub enum PluginError {
-    // #[error("Not found: {0:?}")]
-    // NotFound(String),
-    // #[error("Not implemented: {0:?}")]
-    // NotImplemented(String),
-    // #[error("Invalid input: {0:?}")]
-    // InvalidInput(String),
-    // #[error("Execution Error: {0:?}")]
-    // Execution(String),
-    // #[error("Config error: {0:?}")]
-    // Config(String),
-    // #[error("Could not activate: {0:?}")]
-    // Activation(String),
-    // #[error("Could not initialize: {0:?}")]
-    // Initialization(String),
     #[error("Could not initialize: {0:?}")]
     InternalServer(#[from] app_core::CoreError),
 }
