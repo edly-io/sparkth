3f078a2daaa68ed948b580d0148d8b21be3c40cc
Zaira Bibi <zaira.bibi@A006-01773.local>
2025-10-30 15:02:02 +0500

    fix: fomatting

--- backend/app_core/src/lib.rs
+++ backend/app_core/src/lib.rs
@@ -2,6 +2,8 @@ pub mod db;
 mod schema;
 pub mod service;
 pub mod utils;
-pub use db::{CoreError, DbPool, NewPlugin, NewUser, Plugin, PluginManifest, PluginType, User, get_db_pool,};
-pub use utils::{check_user_exists, validate_email};
+pub use db::{
+    CoreError, DbPool, NewPlugin, NewUser, Plugin, PluginManifest, PluginType, User, get_db_pool,
+};
 pub use service::PluginService;
+pub use utils::{check_user_exists, validate_email};
