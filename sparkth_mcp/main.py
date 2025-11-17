2b277569ff5e01514980f0f4477145037e80ed25
Zaira Bibi <zaira.bibi@A006-01773.local>
2025-11-17 16:23:06 +0500

    feat: support http and stdio modes for mcp server
    The mcp server can now be run on stdio or http transport modes, specified
    through the command line. For the http mode, the host and port are also
    configurable.
    
    Also renamed the mcp module to sparkth_mcp, since it caused name
    conflicts with FastMCP.

---
+++ sparkth_mcp/main.py
@@ -0,0 +1,36 @@
+import argparse
+from fastmcp import FastMCP
+from sparkth_mcp.mode import TransportMode
+
+mcp = FastMCP("Sparkth")
+
+def run_stdio():
+    mcp.run() 
+
+def run_http(host, port):
+    mcp.run(transport="http", host=host, port=port)
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    return a + b
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--transport", 
+        default="http", 
+        choices=[mode.value for mode in TransportMode],
+        help="MCP server transport mode"
+    )
+    parser.add_argument("--host", default="127.0.0.1", help="MCP server host")
+    parser.add_argument("--port", type=int, default=8000, help="MCP server port")
+
+    args = parser.parse_args()
+    transport_mode = TransportMode(args.transport)
+
+    if transport_mode == TransportMode.STDIO:
+        run_stdio()
+    elif transport_mode == TransportMode.HTTP:
+        run_http(args.host, args.port)
+    
