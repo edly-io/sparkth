c578cdf60f01e4d9b96be322381267cbf6f2650a
Abdul Rafey <abdul.rafey1@A006-01536.local>
2026-06-01 15:59:45 +0500

    refactor(googledrive): use os.getenv in constants per project convention

--- app/core_plugins/googledrive/constants.py
+++ app/core_plugins/googledrive/constants.py
@@ -2,4 +2,4 @@
 
 import os
 
-DRIVE_MAX_UPLOAD_BYTES = int(os.environ.get("DRIVE_MAX_UPLOAD_BYTES", str(30 * 1024 * 1024)))
+DRIVE_MAX_UPLOAD_BYTES = int(os.getenv("DRIVE_MAX_UPLOAD_BYTES", str(30 * 1024 * 1024)))
