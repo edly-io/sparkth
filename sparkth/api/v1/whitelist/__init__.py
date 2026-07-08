9373b72781d99884cb7647691fc7d43da8e8314f
Abdul Rafey <abdul.rafey1@arbisoft.com>
2026-07-08 20:36:07 +0500

    refactor(whitelist): convert route and service modules into packages

---
+++ sparkth/api/v1/whitelist/__init__.py
@@ -0,0 +1,5 @@
+"""Email-whitelist API package."""
+
+from sparkth.api.v1.whitelist.routes import router
+
+__all__ = ["router"]
---
+++ sparkth/services/whitelist/__init__.py
@@ -0,0 +1,5 @@
+"""Email-whitelist service package."""
+
+from sparkth.services.whitelist.service import WhitelistService
+
+__all__ = ["WhitelistService"]
